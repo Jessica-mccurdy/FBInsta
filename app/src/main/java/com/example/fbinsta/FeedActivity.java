e15d9ba06297ec6d2b0893b392f5b34610339825
jessica-mccurdy <jessica.mccurdy@yale.edu>
2019-07-09 15:56:37 -0700

    item_post xml set up -> need to create an adapter for recyclerView

---
+++ app/src/main/java/com/example/fbinsta/FeedActivity.java
@@ -0,0 +1,7 @@
+package com.example.fbinsta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FeedActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/example/fbinsta/FeedAdapter.java
@@ -0,0 +1,4 @@
+package com.example.fbinsta;
+
+public class FeedAdapter {
+}
